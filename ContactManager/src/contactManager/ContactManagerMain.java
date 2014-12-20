285961bb99875b402e53d7ca8e005c9167bef0a5
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-20 16:54:13 +0000

    1s commit of old cw from new local location

---
+++ ContactManager/src/contactManager/ContactManagerMain.java
@@ -0,0 +1,12 @@
+package contactManager;
+
+public class ContactManagerMain {
+
+	public static void main(String args[]) {
+		new ContactManagerMain();
+	}
+
+	public ContactManagerMain() {
+		new ContactManagerImpl();
+	}
+}
---
+++ ContactManager/src/contactManager/FutureMeeting.java
@@ -0,0 +1,9 @@
+package contactManager;
+
+/**
+ * A meeting to be held in the future
+ * No methods here, this is just a naming interface
+ * (i.e. only necessary for type checking and/or downcasting)  
+ */
+public interface FutureMeeting extends Meeting {
+}
